90ba51f8da7cb76b5ed251c5de3d454c9e80a46c
shungo0222 <kimura.shungo@gmail.com>
2023-05-07 00:52:06 +0900

    Loading your Hello World smart contract

--- frontend/src/util/interact.js
+++ frontend/src/util/interact.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey); 
+const web3 = createAlchemyWeb3(alchemyKey);
 
-//export const helloWorldContract;
+const contractABI = require("../contract-abi.json");
+const contractAddress = process.env.CONTRACT_ADDRESS;
+
+export const helloWorldContract = new web3.eth.Contract(
+	contractABI,
+	contractAddress
+);
 
 export const loadCurrentMessage = async () => { 
   

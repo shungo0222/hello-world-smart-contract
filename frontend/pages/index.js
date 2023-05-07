a197880a42b8aea09fbd17eab4e6b71f04ed737b
shungo0222 <kimura.shungo@gmail.com>
2023-05-08 00:35:01 +0900

    Implement addWalletListener

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -30,8 +30,29 @@ export default function Home() {
     });
   }
 
-  function addWalletListener() { //TODO: implement
-    
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
   }
 
   const connectWalletPressed = async () => {
@@ -59,6 +80,7 @@ export default function Home() {
       setStatus(status); 
     }
     fetchWallet();
+    addWalletListener();
   }, []);
 
   //the UI of our component

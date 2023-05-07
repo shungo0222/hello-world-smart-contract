7333831b4898ea69d8038a6009f777d87f1f9e84
shungo0222 <kimura.shungo@gmail.com>
2023-05-08 00:18:05 +0900

    The connectWallet function

--- frontend/util/interact.js
+++ frontend/util/interact.js
@@ -16,7 +16,39 @@ export const loadCurrentMessage = async () => {
 };
 
 export const connectWallet = async () => {
-	
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        status: "👆🏽 Write a message in the text-field above.",
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
 };
 
 export const getCurrentWalletConnected = async () => {

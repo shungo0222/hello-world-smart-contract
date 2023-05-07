80907e80abd06cae7fc3c69e0be704069bd88165
shungo0222 <kimura.shungo@gmail.com>
2023-05-08 00:50:57 +0900

    Signing our transaction

--- frontend/util/interact.js
+++ frontend/util/interact.js
@@ -94,6 +94,7 @@ export const getCurrentWalletConnected = async () => {
 };
 
 export const updateMessage = async (address, message) => {
+	// Input error handling
   if (!window.ethereum || address === null) {
     return {
       status:
@@ -104,6 +105,38 @@ export const updateMessage = async (address, message) => {
   if (message.trim() === "") {
     return {
       status: "❌ Your message cannot be an empty string.",
+    };
+  }
+
+	//set up transaction parameters
+	const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: address, // must match user's active address.
+    data: helloWorldContract.methods.update(message).encodeABI(),
+  };
+
+//sign the transaction
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      status: (
+        <span>
+          ✅{" "}
+          <a target="_blank" href={`https://goerli.etherscan.io/tx/${txHash}`}>
+            View the status of your transaction on Etherscan!
+          </a>
+          <br />
+          ℹ️ Once the transaction is verified by the network, the message will
+          be updated automatically.
+        </span>
+      ),
+    };
+  } catch (error) {
+    return {
+      status: "😥 " + error.message,
     };
   }
 };
